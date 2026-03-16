f6c25f88e83370a877d22aff9a9fa8192fa16e8a
Elvis Henrique Pereira <elvishp2006@gmail.com>
2026-03-16 14:03:03 -0300

    fix: icons

--- vite.config.ts
+++ vite.config.ts
@@ -20,10 +20,14 @@ export default defineConfig({
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+          { src: 'icon-maskable-192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
+          { src: 'icon-maskable-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
         ],
       },
-      workbox: { globPatterns: ['**/*.{js,css,html,png,svg,ico}'] },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+        globIgnores: ['splash/*.png', 'brand/*.svg'],
+      },
     }),
   ],
 });
